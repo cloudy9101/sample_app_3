74a108bbc36466a1b99015df2728205941c30949
cloudy9101 <cloudy9101@163.com>
2013-12-26 10:06:30 +0800

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  subject{ page }
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  describe "Home page" do
+    before { visit '/static_pages/home' }
+
+  	it { should have_content('Sample App') }
+    it { should have_title("#{base_title} | Home") }
+  end
+
+  describe "Help page" do
+    before { visit '/static_pages/help' }
+
+  	it { should have_content('Help') }
+    it { should have_title("#{base_title} | Help") }
+  end
+
+  describe "About page" do
+    before { visit '/static_pages/about' }
+
+    it { should have_content('About Us') }
+    it { should have_title("#{base_title} | About Us") }
+  end
+end
